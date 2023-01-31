00cb02529facda3016684f2d4ef4d619e56c4b68
suzz-in <o_o_su_jin_@naver.com>
2023-01-31 17:45:18 +0900

    recoil 비동기 처리

--- nextjs-recoil/pages/post.js
+++ nextjs-recoil/pages/post.js
@@ -1,31 +1,45 @@
-import { useRecoilState } from "recoil";
-import { pageNameState } from "../states";
+import { useRecoilState, useRecoilValueLoadable } from "recoil";
+import { getNameSelector, pageNameState } from "../states";
 import Link from "next/link";
 
 const PostPage = () => {
   const [pageName, setPageName] = useRecoilState(pageNameState);
-  return (
-    <div>
-      <div>
-        <h1>Post Page!</h1>
-      </div>
-      <div>
-        <span>pageName 상태: {pageName}</span>
-      </div>
-      <div>
-        <button
-          onClick={() => {
-            setPageName("PostPage");
-          }}
-        >
-          현재 페이지 이름으로 상태 변경
-        </button>
-      </div>
-      <Link href="/">
-        <button>Index Pages 이동</button>
-      </Link>
-    </div>
-  );
+  //suspaense 대신해서 recoilvalueLoadable 사용
+  const name = useRecoilValueLoadable(getNameSelector);
+
+  switch (name.state) {
+    case "hasValue":
+      return (
+        <div>
+          <div>
+            <h1>Post Page!</h1>
+          </div>
+          <div>
+            <span>pageName 상태: {pageName}</span>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                setPageName("PostPage");
+              }}
+            >
+              현재 페이지 이름으로 상태 변경
+            </button>
+          </div>
+          <div>
+            <h1>{JSON.stringify(name.contents)}</h1>
+          </div>
+          <Link href="/">
+            <button>Index Pages 이동</button>
+          </Link>
+        </div>
+      );
+
+    case "loading":
+      return <div>Loading</div>;
+    case "hasError":
+      return <div>{name.contents}</div>;
+  }
 };
 
 export default PostPage;
--- nextjs-recoil/states/index.js
+++ nextjs-recoil/states/index.js
@@ -1,6 +1,15 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+import axios from "axios";
 
 export const pageNameState = atom({
   key: "pageNameState",
   default: "",
+});
+
+export const getNameSelector = selector({
+  key: "getNameSelector",
+  get: async () => {
+    const res = await axios.get("http://localhost:3000/api/hello");
+    return res.data; //res 모두 리턴하면 run time error 발생함. (dev환경에서)
+  },
 });
